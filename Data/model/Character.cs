fb7c02f37a961e25aa04a52176577f8d68e3d9d2
Илья <marchenko.ilya99@gmail.com>
2022-12-07 16:19:39 +0200

    Add Data => model => Character

---
+++ Data/model/Character.cs
@@ -0,0 +1,21 @@
+﻿namespace Rick_And_Morty.Data.model
+{
+    public class Character
+    {
+        public Result Result { get; set; }
+    }
+    public class Result
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+        public List<string> episode { get; set; }
+        public string url { get; set; }
+        public DateTime created { get; set; }
+    }
+
+}
