1d5768acd10133f3ff1c347f1a9ace9f8fc734e8
Илья <marchenko.ilya99@gmail.com>
2022-12-07 16:15:56 +0200

    AddController

---
+++ Controllers/DataValidationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rick_And_Morty.Controllers
+{
+    [ApiController]
+    public class DataValidationController : Controller
+    {
+
+        [HttpPost("/api/v1/check-person")]
+        public async Task<IActionResult> checkPerson()
+        {
+            return Ok();
+        }
+        [HttpGet("/api/v1/person")]
+        public async Task<IActionResult> checkPerson(string name)
+        {
+            return Ok();
+        }
+
+    }
+}
